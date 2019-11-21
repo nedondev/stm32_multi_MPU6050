6f1a0dfa8ae1c7e321127e865f2704d72cca3fe9
nedondev <nedondev@gmail.com>
2019-11-21 08:41:09 +0700

    FIX:BUG

--- visualize/python_serial.py
+++ visualize/python_serial.py
@@ -10,7 +10,7 @@ def read_all_filtered_out():
     line_temp = ""
     attributes = []
     data = {}
-    while len(attributes) < 10:
+    while len(attributes) < 6:
         for line in ser.read():
             # print(chr(line)+"("+str(line)+")",end='')
             # print(chr(line),end='')
@@ -20,33 +20,24 @@ def read_all_filtered_out():
                 for attribute in temp_attributes:
                     for sub_attribute in attribute.split("#"):
                         attributes.append(sub_attribute)
-                if len(attributes) < 10 :
+                if len(attributes) < 6 :
                     print(line_temp)
                     #clear if no collect char from a 1st char or not collect all data
                     line_temp = ""
                     break
                 if attributes[0] != "Index" and attributes[2] != "DEL" \
-                    and attributes[4] != "ACC" and attributes[6] != "GYR" \
-                    and attributes[8 != "FIL"]:
+                    and attributes[4] != "FIL":
                     #clear if no collect char from a 1st char or not collect all data
                     line_temp = "" 
                     break
                     pass
-                accel_angle = attributes[5].split(',')
-                gyro_angle = attributes[7].split(',')
-                angle = attributes[9].split(',')
-                if len(accel_angle) + len(gyro_angle) + len(angle) < 9 :
+                angle = attributes[5].split(',')
+                if len(angle) < 3 :
                     break
                 data = {
                     "result": 0,
                     "id": attributes[1],
                     "delta_time":attributes[3],
-                    "gyro_angle_x":gyro_angle[0],
-                    "gyro_angle_y":gyro_angle[1],
-                    "gyro_angle_z":gyro_angle[2],
-                    "accel_angle_x":accel_angle[0],
-                    "accel_angle_y":accel_angle[1],
-                    "accel_angle_z":accel_angle[2],
                     "angle_x":angle[0],
                     "angle_y":angle[1],
                     "angle_z":angle[2]
@@ -72,33 +63,24 @@ if __name__ == '__main__':
                 for attribute in temp_attributes:
                     for sub_attribute in attribute.split("#"):
                         attributes.append(sub_attribute)
-                if len(attributes) < 10 :
+                if len(attributes) < 6 :
                     print(line_temp)
                     #clear if no collect char from a 1st char or not collect all data
                     line_temp = ""
                     break
                 if attributes[0] != "Index" and attributes[2] != "DEL" \
-                    and attributes[4] != "ACC" and attributes[6] != "GYR" \
-                    and attributes[8 != "FIL"]:
+                    and attributes[4] != "FIL":
                     #clear if no collect char from a 1st char or not collect all data
                     line_temp = "" 
                     break
                     pass
-                accel_angle = attributes[5].split(',')
-                gyro_angle = attributes[7].split(',')
-                angle = attributes[9].split(',')
-                if len(accel_angle) + len(gyro_angle) + len(angle) < 9 :
+                angle = attributes[5].split(',')
+                if len(angle) < 3 :
                     break
                 data = {
                     "result": 0,
                     "id": attributes[1],
                     "delta_time":attributes[3],
-                    "gyro_angle_x":gyro_angle[0],
-                    "gyro_angle_y":gyro_angle[1],
-                    "gyro_angle_z":gyro_angle[2],
-                    "accel_angle_x":accel_angle[0],
-                    "accel_angle_y":accel_angle[1],
-                    "accel_angle_z":accel_angle[2],
                     "angle_x":angle[0],
                     "angle_y":angle[1],
                     "angle_z":angle[2]
